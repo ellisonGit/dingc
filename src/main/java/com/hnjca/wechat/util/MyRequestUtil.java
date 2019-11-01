9f7787b130f9257ab0c46a2d6a16677a7880158e
ellison <13711853447@.com>
2019-11-01 16:26:35 +0800

    更新util file

--- src/main/java/com/hnjca/wechat/util/MyRequestUtil.java
+++ src/main/java/com/hnjca/wechat/util/MyRequestUtil.java
@@ -1,9 +1,6 @@
 package com.hnjca.wechat.util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -81,22 +78,25 @@ public class MyRequestUtil {
 	            // 打开和URL之间的连接
 	            URLConnection conn = realUrl.openConnection();
 	            // 设置通用的请求属性
+				conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
 	            conn.setRequestProperty("accept", "*/*");
 	            conn.setRequestProperty("connection", "Keep-Alive");
 	            conn.setRequestProperty("user-agent",
 	                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+				conn.setRequestProperty("Accept-Charset", "utf-8");
+				//conn.setRequestProperty("contentType", "utf-8");
 	            // 发送POST请求必须设置如下两行
 	            conn.setDoOutput(true);
 	            conn.setDoInput(true);
 	            // 获取URLConnection对象对应的输出流
-	            out = new PrintWriter(conn.getOutputStream());
+				out = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(), "utf-8"));
 	            // 发送请求参数
 	            out.print(param);
 	            // flush输出流的缓冲
 	            out.flush();
 	            // 定义BufferedReader输入流来读取URL的响应
-	            in = new BufferedReader(
-	                    new InputStreamReader(conn.getInputStream()));
+				in = new BufferedReader(
+						new InputStreamReader(conn.getInputStream(), "utf-8"));
 	            String line;
 	            while ((line = in.readLine()) != null) {
 	                result += line;
@@ -120,6 +120,6 @@ public class MyRequestUtil {
 	            }
 	        }
 	        return result;
-	    }    
-	  
+	    }
+
 }
